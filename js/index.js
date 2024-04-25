73ad6478e56ed0035900f57a8311151810c372d2
AenukaTeam <aenuka.team@gmail.com>
2024-04-25 07:56:04 +0530

    Simeple Count program using Javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("Lets Develop Our assignement using Javascript");
+let count = 0;
+
+function increament() {
+    count++;
+    console.log(count);
+    document.getElementById("h1Count").innerText = count;
+}
+
+function reset() {
+    count = 0;
+    console.log(count);
+    document.getElementById("h1Count").innerText = count;
+}
